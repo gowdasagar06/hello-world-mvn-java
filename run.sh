842932e4a1f81b7a6a906c2c810c86c790e841b9
gowdasagar06 <92599815+gowdasagar06@users.noreply.github.com>
2024-06-07 11:23:14 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 if [[ $run_sonar == "YES" ]]; then
   echo "Running SonarQube analysis..."
   mvn clean verify sonar:sonar \
-    -Dsonar.projectKey=demo \
-    -Dsonar.projectName='demo' \
+    -Dsonar.projectKey=demo-dev \
+    -Dsonar.projectName='demo-dev' \
     -Dsonar.host.url=http://13.235.100.209:9000 \
     -Dsonar.token=squ_2f217b09857d7c2af399d2b22f807de615667451
 else
